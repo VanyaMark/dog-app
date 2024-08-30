a9e7d419aa1b45bcf6f53b997038feb1b503d5d6
VanyaMark <markova.vanya@gmail.com>
2024-08-30 12:34:36 +0200

    created three media queries adjusting the card size, added logo and favicon

--- index.js
+++ index.js
@@ -1,52 +1,66 @@
 const BREEDS_URL = "https://dog.ceo/api/breeds/list/all";
 const select = document.querySelector('.breeds');
+const img = document.querySelector('.dog-img');
+const dogImgTitle = document.querySelector('.dog-img-title');
+const loader = document.querySelector('.loader');
 
+// Fetch the list of all dog breeds from the Dog CEO API and populate the <select> dropdown
 fetch(BREEDS_URL)
     .then(response => {
-        return response.json()
+        // Convert the API response to JSON format
+        return response.json();
     })
     .then(data => {
+        // Extract the breeds object from the API response
         const breedsObject = data.message;
+        // Get an array of breed names from the object keys
         const breedsArray = Object.keys(breedsObject);
-    //    console.log(Object.keys(data.message))
 
+        // Iterate through the array of breeds and create <option> elements for each breed
         for (let i = 0; i < breedsArray.length; i++) {
             const option = document.createElement('option');
+            // Set the value and inner text of each option to the breed name
             option.value = breedsArray[i];
             option.innerText = breedsArray[i];
-            select.appendChild(option)
+            // Append each option to the <select> dropdown
+            select.appendChild(option);
         }
-    })
+    });
 
-    select.addEventListener('change', event => {
+// Listen for changes in the <select> dropdown and update the dog image based on the selected breed
+select.addEventListener('change', event => {
+    // Get the value of the selected breed from the event object
+    let selectedBreed = event.target.value;
+    // Fetch and display a random image of the selected breed
+    getDoggo(selectedBreed);
+});
 
-        let url = `https://dog.ceo/api/breed/${event.target.value}/images/random`;
+// Fetch a random image of the selected breed from the Dog CEO API and update the page
+const getDoggo = selectedBreed => {
+    // Construct the API URL for fetching a random image of the selected breed
+    let url = `https://dog.ceo/api/breed/${selectedBreed}/images/random`;
 
-        getDoggo(url);
-    })
+    // Show the loader and hide the previous dog image while the new image is being fetched
+    loader.classList.add('show');
+    img.classList.remove('show');
 
-    const img = document.querySelector('.dog-img');
-    // const spinner =  document.querySelector('.spinner');
+    // Fetch the image from the API
+    fetch(url)
+        .then(response => {
+            // Convert the API response to JSON format
+            return response.json();
+        })
+        .then(data => {
+            // Set the <img> src to the fetched dog image URL
+            img.src = data.message;
+        });
+    
+    // Update the image title with the selected breed name in uppercase
+    dogImgTitle.innerHTML = selectedBreed.toUpperCase();
+};
 
-    const loader = document.querySelector('.loader');
-
-    const getDoggo = url => {
-        // spinner.classList.add('show');
-        loader.classList.add('show');
-        img.classList.remove('show');
-
-        fetch(url)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                img.src = data.message;
-
-            })
-    }
-
-    img.addEventListener('load', ()=> {
-        // spinner.classList.remove('show');
-        loader.classList.remove('show');
-        img.classList.add('show');
-    })
+// Hide the loader and show the new dog image once it has fully loaded
+img.addEventListener('load', () => {
+    loader.classList.remove('show');
+    img.classList.add('show');
+});
